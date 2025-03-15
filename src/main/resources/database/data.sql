3d37aea6fbe417ce71b8ed6a0aaf9a09806e6b7e
QuocBao209 <baobao2092004@gmail.com>
2025-03-15 18:35:59 +0700

    thêm folder db ở resources với file data.sql

---
+++ src/main/resources/database/data.sql
@@ -0,0 +1,6 @@
+-- src/main/resources/database/data.sql
+INSERT INTO NDT (maNDT, hoTen, ngaySinh, mkGiaoDich, diaChi, phone, cmnd, gioiTinh, email)
+VALUES ('NDT001', N'Nguyễn Văn A', '1990-01-01', NULL, N'123 Đường Láng, Hà Nội', '0123456789', '1234567890', N'Nam', 'nguyenvana@example.com');
+
+INSERT INTO NDT (maNDT, hoTen, ngaySinh, mkGiaoDich, diaChi, phone, cmnd, gioiTinh, email)
+VALUES ('NDT002', N'Trần Thị B', '1995-05-15', NULL, N'456 Nguyễn Trãi, TP.HCM', '0987654321', '0987654321', N'Nữ', 'tranthib@example.com');
