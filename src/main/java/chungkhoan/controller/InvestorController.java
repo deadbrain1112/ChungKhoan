d1d7bbc8e04e95a58616c7ff0fc8d40eb91df1ff
QuocBao209 <baobao2092004@gmail.com>
2025-04-08 19:19:18 +0700

    Mới xử lý investor_list.html thôi, controller xử lý là method investorList

--- src/main/java/chungkhoan/controller/InvestorController.java
+++ src/main/java/chungkhoan/controller/InvestorController.java
@@ -1,33 +1,43 @@
 package chungkhoan.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import chungkhoan.entity.NhaDauTu;
+import chungkhoan.repository.NDTRepository;
 
 @Controller
 public class InvestorController {
 	
+	@Autowired
+	private NDTRepository ndtRepository;
+	
 	@GetMapping("/investors")
-    public String investorsList() {
-    	return "investor_list";
+    public String investorsList(Model model) {
+		 model.addAttribute("investors", ndtRepository.findAll());
+	     model.addAttribute("investor", new NhaDauTu()); // cho form Thêm
+	     return "nhanvien/investor_list";
     }
 	
 	// Xử lý xóa nhà đầu tư ( xóa theo id trên đường dẫn )
 	@PostMapping("/investors/delete/{maNDT}")
 	public String deleteInvestor () {
-		return "redirect:/investos_list";
+		return "redirect:/investors";
 	}
 	
 	// Xử lý thêm nhà đầu từ khi bấm Thêm. Bấm thêm --> tự đổi form action là /investors/add ( dòng 138 trong investor_list )
 	@PostMapping("/investors/add")
 	public String addInvestor() {
-		return "redirect:/investor_list";
+		return "redirect:/investors";
 	}
 	
 	// Xử lý chỉnh sửa nhà đầu từ khi bấm Sửa. Bấm sửa --> tự đổi form action là /investors/edit + maNDT ( dòng 146 trong investor_list )
 	@PostMapping("/investors/edit/{maNDT}")
 	public String editInvestor() {
-		return "redirect:/investor_list";
+		return "redirect:/investors";
 	}
 	
 }
