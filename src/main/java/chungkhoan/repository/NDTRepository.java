fffd07972889d4fdf198442446f55eed244e5b40
deadbrain1112 <127726225+deadbrain1112@users.noreply.github.com>
2025-04-09 21:15:34 +0700

    chỉnh lại thêm cổ phiểu, chờ thêm cột mkgiaodich mới làm tiếp được

--- src/main/java/chungkhoan/repository/NDTRepository.java
+++ src/main/java/chungkhoan/repository/NDTRepository.java
@@ -2,25 +2,32 @@ package chungkhoan.repository;
 
 import chungkhoan.entity.NhaDauTu;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.query.Procedure;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.sql.Date;
 import java.util.List;
 
 public interface NDTRepository extends JpaRepository<NhaDauTu, String> {
     NhaDauTu findByMaNDT(String maNDT);
     List<NhaDauTu> findAll();
 
-    @Procedure(name = "sp_ThemNhaDauTu")
+    @Modifying
+    @Transactional
+    @Query(value = "EXEC sp_ThemNhaDauTu :maNDT, :hoTen, :ngaySinh, :mkgd, :diaChi, :phone, :cmnd, :gioiTinh, :email", nativeQuery = true)
     void themNhaDauTu(
-            @Param("MaNDT") String maNDT,
-            @Param("HoTen") String hoTen,
-            @Param("NgaySinh") java.sql.Date ngaySinh,
-            @Param("MKGD") String mkgd,
-            @Param("DiaChi") String diaChi,
-            @Param("Phone") String phone,
-            @Param("CMND") String cmnd,
-            @Param("GioiTinh") String gioiTinh,
-            @Param("Email") String email
+            @Param("maNDT") String maNDT,
+            @Param("hoTen") String hoTen,
+            @Param("ngaySinh") Date ngaySinh,
+            @Param("mkgd") String mkgd,
+            @Param("diaChi") String diaChi,
+            @Param("phone") String phone,
+            @Param("cmnd") String cmnd,
+            @Param("gioiTinh") String gioiTinh,
+            @Param("email") String email
     );
+
 }
