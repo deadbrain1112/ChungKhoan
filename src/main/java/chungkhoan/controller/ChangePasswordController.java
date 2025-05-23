206fafda9d590557a0695f94bcfd6d7778eeecdb
QuocBao209 <baobao2092004@gmail.com>
2025-05-23 15:01:20 +0700

    Thêm file change_password.html, ChangePasswordController, đã xử lý controller hiện view

---
+++ src/main/java/chungkhoan/controller/ChangePasswordController.java
@@ -0,0 +1,31 @@
+package chungkhoan.controller;
+
+import chungkhoan.repository.NDTRepository;
+import chungkhoan.repository.NhanVienRepository;
+import chungkhoan.service.TaiKhoanService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ChangePasswordController {
+
+    private final NhanVienRepository nhanVienRepository;
+    private final NDTRepository ndtRepository;
+    private final TaiKhoanService taiKhoanService;
+
+    public ChangePasswordController(NhanVienRepository nhanVienRepository,
+                                    NDTRepository ndtRepository,
+                                    TaiKhoanService taiKhoanService) {
+        this.nhanVienRepository = nhanVienRepository;
+        this.ndtRepository = ndtRepository;
+        this.taiKhoanService = taiKhoanService;
+    }
+
+    @GetMapping("/change-password")
+    public String hienThiFormDoiMatKhau(Model model) {
+        model.addAttribute("nhanViens", nhanVienRepository.findAll());
+        model.addAttribute("nhaDauTus", ndtRepository.findAll());
+        return "nhanvien/change_password"; 
+    }
+}
